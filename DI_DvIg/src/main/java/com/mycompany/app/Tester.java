9ae89dcbda7cae3b30563f53897a39a38bf750cf
dvorig3 <idvorskij@mail.ru>
2015-02-17 23:54:09 +0200

    cr new f

--- DI_DvIg/src/main/java/com/mycompany/app/Tester.java
+++ DI_DvIg/src/main/java/com/mycompany/app/Tester.java
@@ -1,40 +1,40 @@
 package com.mycompany.app;
 
-import java.util.Map;
 
 public class Tester {
 
 	public static void main(String[] args) {
 
+		
 		// Testing Fruits
-		IFruit fruit = (IFruit) StaticInfo.CONTEXT.getBean("unknfr");
+		IFruit fruit = StaticInfo.CONTEXT.getBean("unknfr", IFruit.class);
 		System.out.println(fruit.getName());
-		IFruit apple = (IFruit) StaticInfo.CONTEXT.getBean("apple");
+		IFruit apple = StaticInfo.CONTEXT.getBean("apple", IFruit.class);
 		System.out.println(apple.getName());
-		IFruit orange = (IFruit) StaticInfo.CONTEXT.getBean("orange");
+		IFruit orange = StaticInfo.CONTEXT.getBean("orange", IFruit.class);
 		System.out.println(orange.getName());
-		IFruit melon = (IFruit) StaticInfo.CONTEXT.getBean("melon");
+		IFruit melon = StaticInfo.CONTEXT.getBean("melon", IFruit.class);
 		System.out.println(melon.getName());
 		
 		// Testing FruitSlicer
 		{
 			System.out.println("\n> Test instance of FruitSlicer created with a constructor by default");
-			ISlicer fruitSlicer = (FruitSlicer) StaticInfo.CONTEXT
-					.getBean("frSlNoArgs");
+			ISlicer fruitSlicer = StaticInfo.CONTEXT
+					.getBean("frSlNoArgs", FruitSlicer.class);
 			fruitSlicer.slice();
 			fruitSlicer.multySlice();
 		}
 		{
 			System.out.println("\n> Test instance of FruitSlicer created with a constructor with parameter");
-			ISlicer fruitSlicer = (FruitSlicer) StaticInfo.CONTEXT
-					.getBean("frSlArg");
+			ISlicer fruitSlicer = StaticInfo.CONTEXT
+					.getBean("frSlArg", FruitSlicer.class);
 			fruitSlicer.slice();
 			fruitSlicer.multySlice();
 		}
 		{
 			System.out.println("\n> Test instance of FruitSlicer created with a constructor by default and properties");
-			ISlicer fruitSlicer = (FruitSlicer) StaticInfo.CONTEXT
-					.getBean("frSlProp");
+			ISlicer fruitSlicer = StaticInfo.CONTEXT
+					.getBean("frSlProp", FruitSlicer.class);
 			fruitSlicer.slice();
 			fruitSlicer.multySlice();
 		}
@@ -42,8 +42,8 @@ public class Tester {
 		// Testing FruitSlicerCombain
 		{
 			System.out.println("\n> Test instance of FruitSlicerCombain created with a constructor by default");
-			ISlicer fruitSlicer = (FruitSlicer) StaticInfo.CONTEXT
-					.getBean("frSlComNoArgs");
+			ISlicer fruitSlicer = StaticInfo.CONTEXT
+					.getBean("frSlComNoArgs", FruitSlicer.class);
 			fruitSlicer.slice();
 			fruitSlicer.multySlice();
 		}
